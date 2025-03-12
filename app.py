948f23385f20867526828aa9fe222b7d05b39787
Fraida Fund <ffund@nyu.edu>
2025-03-12 11:44:18 -0400

    Update resize

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ FOOD11_MODEL_NAME=os.environ['FOOD11_MODEL_NAME']
 
 def preprocess_image(img):
     transform = transforms.Compose([
-        transforms.Resize(224, 224),
+        transforms.Resize((224, 224)),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
